9f2e437620af740ad5487db85844cde003e77eeb
S. Joshua Swamidass <swamidass@gmail.com>
2022-02-25 12:53:44 -0600

    upgrade to algolia 4

--- src/utils/getRemoteIndex.js
+++ src/utils/getRemoteIndex.js
@@ -1,30 +1,11 @@
-module.exports = async function getRemoteIndex(index) {
-    var oldIndex 
-    try {
-        oldIndex = await query(index)
-    } catch (err) {
-        throw err
-    }
-
-    return oldIndex
-}
-
-function query(index) {
-    return new Promise(function(resolve, reject) {
-        let hits = []; 
-        index.browse("", {}, function browseDone(err, content) {
-            if (err) return reject(err);
-
-            content.hits.forEach(function(hit) {
-                hit.objectID = String(hit.objectID)
-                hits.push(hit)
-            })
-
-            if (content.cursor) {
-                index.browseFrom(content.cursor, browseDone);
-            } else {
-                resolve(hits)
-            }
-        })
-    })
+module.exports = async function  getRemoteIndex(index) {
+  let hits = [];
+  await index.browseObjects({
+    query: '', // Empty query will match all records
+    batch: batch => {
+      hits = hits.concat(batch);
+    },
+    attributesToRetrieve: ["*"]
+  })
+  return hits
 }
